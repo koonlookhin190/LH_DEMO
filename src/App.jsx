a8037cce4d0120372c947eec23096c6752a26fd8
koonlookhin <krit_tipnuan@cmu.ac.th>
2024-01-12 16:55:28 +0700

    add home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 // import viteLogo from '/vite.svg'
 import './App.css'
 import ShakeGame from './pages/ShakeGame'
+import Home from './pages/Home'
 import RepeatGame from './pages/RepeatGame'
 import DotGame from './pages/DotGame'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -11,29 +12,10 @@ function App() {
 
   return (
     <>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
      <BrowserRouter>
       <Routes>
-          <Route index element={<ShakeGame />} />
+          <Route index element={<Home />} />
+          <Route path="/shakegame" element={<ShakeGame />} />
           <Route path="/repeat" element={<RepeatGame />} />
           <Route path="/dotgame" element={<DotGame />} />
       </Routes>
